4b90e3662f29c30df36b18edaf85737e6f5fd2b5
rahmed19 <ray.ahmed.sec@gmail.com>
2021-03-18 05:46:14 -0700

    Eslint Prettier Install

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Jumbotron from './components/jumbotron'
 import jumboData from './fixtures/jumbo'
- 
 
 export default function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app'; 
+import GlobalStyles from './global-styles'
+import 'normalize.css'
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<> 
+<GlobalStyles />
+<App />
+</>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
