9fd7b9a82abea48004d7430ea8084103149faf81
rahmed19 <ray.ahmed.sec@gmail.com>
2021-03-27 15:54:55 -0700

    Signin Form Setup

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,16 +1,55 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { HeaderContainer } from '../containers/header'
 import { FooterContainer } from '../containers/footer'
+import { Form } from '../components/'
 
 export default function Signin() {
+    const [emailAddress, setEmailAddress] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+
+    const isInValid = password == '' || emailAddress == ''
+    const handleSignIn = (e) => {
+        e.preventDefault()
+
+        //firebase work
+    }
+
     return (
     <>
     <HeaderContainer>
-        hello from sign in
+    <Form>
+        <Form.Title>Sign In</Form.Title>
+
+        {error && <Form.Error>{error}</Form.Error>}
+
+        <Form.Base onSubmit={handleSignIn} method="POST">
+            <Form.Input 
+                placeholder="Email Address"
+                value={emailAddress}
+                onChange={({target}) => setEmailAddress(target.value)}
+            />
+            
+            <Form.Input 
+                placeholder="Password"
+                value={password}
+                type="password"
+                autocomplete="off"
+                onChange={({target}) => setPassword(target.value)}
+            />  
+        <Form.Submit disabled={isInValid} type="submit">
+                Sign In
+            </Form.Submit>
+         </Form.Base>
+         <Form.Text>
+             New to Netflix? <Form.Link to="/signup">Sign up now</Form.Link>
+         </Form.Text>
+         <Form.TextSmall>
+             This page is protexted by Google reCAPTCHAto ensure you're not a bot. Learn more.
+         </Form.TextSmall>
+     </Form>    
     </HeaderContainer>
-    <FooterContainer>
-         
-    </FooterContainer>
+    <FooterContainer />
     </>
     )
 }
