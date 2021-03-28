0641ab9fac06540b2f003266379760ff9e834b3c
rahmed19 <ray.ahmed.sec@gmail.com>
2021-03-27 18:08:54 -0700

    Signin Page Complete

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,9 +1,14 @@
-import React, {useState} from 'react'
+import React, { useState, useContext } from 'react'
+import { useHistory } from 'react-router-dom'
+import { FirebaseContext } from '../context/firebase'
 import { HeaderContainer } from '../containers/header'
 import { FooterContainer } from '../containers/footer'
 import { Form } from '../components/'
+import * as ROUTES from '../constants/routes'
 
 export default function Signin() {
+    const history = useHistory()
+    const { firebase } = useContext(FirebaseContext) 
     const [emailAddress, setEmailAddress] = useState('')
     const [password, setPassword] = useState('')
     const [error, setError] = useState('')
@@ -13,6 +18,18 @@ export default function Signin() {
         e.preventDefault()
 
         //firebase work
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(emailAddress, password)
+            .then(()=>{
+                history.push(ROUTES.BROWSE)
+            })
+            .catch((error) => {
+                setEmailAddress('')
+                setPassword('')
+                setError(error.message)
+            })
+
     }
 
     return (
