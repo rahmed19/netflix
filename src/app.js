abfee67dc5ddb15158ea9b67f196924b973de252
rahmed19 <ray.ahmed.sec@gmail.com>
2021-03-28 17:29:31 -0700

    Is User Redirect Complete

--- src/app.js
+++ src/app.js
@@ -1,25 +1,50 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import {Home, Browse, Signin, Signup } from './pages'
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes'
 
 import * as ROUTES from './constants/routes'
 
 
 export default function App() {
+  const user = {}
   return (
     <Router>
-       <Route exact path = {ROUTES.BROWSE} >
-          <Browse />
-      </Route> 
-       <Route exact path = {ROUTES.SIGN_UP} >
-          <Signup />
-      </Route> 
-      <Route exact path = {ROUTES.SIGN_IN} >
+     <IsUserRedirect 
+        user={user} 
+        loggedInPath={ROUTES.BROWSE} 
+        path={ROUTES.SIGN_IN}
+        exact
+        >
           <Signin />
-      </Route> 
-      <Route exact path = {ROUTES.HOME} >
-          <Home />
-      </Route> 
+      </IsUserRedirect>
+
+      <IsUserRedirect 
+        user={user} 
+        loggedInPath={ROUTES.BROWSE} 
+        path={ROUTES.SIGN_UP}
+        exact
+      >
+          <Signup />
+      </IsUserRedirect>  
+      
+      <ProtectedRoute
+        user={user}
+        path={ROUTES.BROWSE}
+        exact
+      >
+        <Browse />
+      </ProtectedRoute>
+
+      <IsUserRedirect 
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.HOME}
+        exact
+      >
+        <Home />
+      </IsUserRedirect>
+
     </Router>
   )
 }
