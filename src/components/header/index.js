8baa33775ce8738f58fe7141857b95a8ca38ad01
rahmed19 <ray.ahmed.sec@gmail.com>
2021-04-07 22:40:20 -0700

    Header Frame Group TextLink

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link as ReactRouterLink } from 'react-router-dom'
-import {Background, Container, Logo, ButtonLink, Feature, Text} from './styles/header'
+import {Background, Container, Logo, ButtonLink, Feature, Text, FeatureCallOut, Link, Group} from './styles/header'
 
 export default function Header({bg=true, children, ...restProps}){
     return bg ? <Background {...restProps}>{children}</Background> : children
@@ -14,8 +14,20 @@ Header.Text = function HeaderText({ children, ...restProps}) {
     return <Text {...restProps}>{children}</Text>
 }
 
+Header.TextLink = function HeaderTextLink({children, ...restProps}) {
+    return <Link {...restProps}>{children}</Link>
+}
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({ children, ...restProps}) {
+    return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>
+}
+
 Header.Frame = function HeaderFrame ({children, ...restProps}) {
     return <Container {...restProps}>{children}</Container>
+}
+
+Header.Group = function HeaderGroup ({children, ...restProps}) {
+    return <Group {...restProps}>{children}</Group>
 }
 
 Header.Logo = function HeaderLogo ({to, ...restProps}){
