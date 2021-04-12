644125aca4e34eaa98e05c8e3d8cf9299dedeb77
rahmed19 <ray.ahmed.sec@gmail.com>
2021-04-11 17:06:33 -0700

    firebase fi

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -4,7 +4,12 @@ import 'firebase/auth'
 
 
 const config = {
-   
+    apiKey: "AIzaSyBYKiKJ73TlEHjOxxLBX35pal_dPmCptN4",
+    authDomain: "netflix-y.firebaseapp.com",
+    projectId: "netflix-y",
+    storageBucket: "netflix-y.appspot.com",
+    messagingSenderId: "278692663204",
+    appId: "1:278692663204:web:1e663eee7e7e7e5b767b31"
 }
 const firebase = Firebase.initializeApp(config)
 
